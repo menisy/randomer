ba37fe91af2deba8a1b168f471cfd6fbc48107e2
Menisy <elmenisy@gmail.com>
2012-08-25 00:00:51 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Randomer::Application.config.secret_token = 'e0112f7ef507edd29ec8d00e02502c3390d7451d50148e8cf18b5091d41a4d25a7f9945f7c61b7879208f632119f89ef098a811624fdf910a4bcb82fcd43dc11'
